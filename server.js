6ed8ab9f531e5f00011a2c80cbb0fa1e3ee64286
Floyd Orr <floydcanhelp@gmail.com>
2019-04-24 07:21:49 -0500

    error handling

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 app.use(cors());
 
+
 function Format (search_query,formattedAddress,latitude,longitude) {
   this.search_query = search_query;
   this.formattedAddress = formattedAddress;
@@ -13,6 +14,13 @@ function Format (search_query,formattedAddress,latitude,longitude) {
   this.longitude = longitude;
 
 }
+
+//not sure about error handling
+app.use(function(err, req, res, next) {
+  console.error(err.message); // Log error message in our server's console
+  if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
+  res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
+});
 
 app.get('/location', (req, res) => {
   let data = require('./data/geo.json');
@@ -23,10 +31,7 @@ app.get('/location', (req, res) => {
   res.send(new Format(search_query,formattedAddress,latitude,longitude));
 });
 let info = require('./data/darksky.json');
-// info.forEach(item => {
-//   console.log(item.daily);
 
-// });
 
 function Weather (forecast,time ) {
   this.forecast = forecast;
@@ -45,32 +50,14 @@ app.get('/weather', (req, res) => {
     var year = a.getFullYear();
     var month = months[a.getMonth()];
     var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
     var day = days[a.getDay()];
     var time = day + ' ' + month + ' '+ date +' ' + year ;
     console.log(new Weather(forecast, time));
     res.send(new Weather(forecast, time));
     //return time;
   }
-  //"Mon Jan 01 2001"
   timeConverter(0);
-  // console.log(new Weather(forecast, time));
-  // res.send(new Weather(forecast, time));
-  //1540018800
-
-//   [
-//     {
-//       "forecast": "Partly cloudy until afternoon.",
-//       "time": "Mon Jan 01 2001"
-//     },
-//     {
-//       "forecast": "Mostly cloudy in the morning.",
-//       "time": "Tue Jan 02 2001"
-//     },
-//     ...
-//   ]
+  
 });
 
 
@@ -78,14 +65,3 @@ app.get('/weather', (req, res) => {
 //http://localhost:3000/location?data=%22what%22
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-// {
-//   "search_query": "seattle",
-//   "formatted_query": "Seattle, WA, USA",
-//   "latitude": "47.606210",
-//   "longitude": "-122.332071"
-// }
-// Create a route with a method of get and a path of /location. The route callback should invoke a function to convert the search query to a latitude and longitude. The function should use the provided JSON data.
-// A constructor function will ensure that each object is created according to the same format when the server receives data. Ensure your code base uses a constructor function for this resource.
-// Return an object which contains the necessary information for correct client rendering. See the sample response.
-// Confirm that your route is responding as expected by entering your deployed backend URL in the input of the deployed static client, then searching for a location.
-// Redeploy your application.
